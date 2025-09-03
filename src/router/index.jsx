ae668cd633e369f55f59aad7229c4667c09149f7
Luis Centeno <luis.centeno@saatvamattress.com>
2025-09-02 21:00:08 -0600

    Wrap routes in Suspense for improved loading experience

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,29 +1,34 @@
+import { lazy, Suspense } from 'react'
 import { Route, Switch } from 'wouter'
-import HomePage from '@/pages/home'
-import LoginPage from '@/pages/login'
-import DashboardPage from '@/pages/dashboard'
-import HistoryPage from '@/pages/history'
-import ProfilePage from '@/pages/profile'
-import SettingsPage from '@/pages/settings'
-import RegisterPage from '@/pages/register'
 import Navigation from '@/components/navigation'
+import FullPageLoader from '@/components/common/fullpage-loader'
+
+const HomePage = lazy(() => import('@/pages/home'))
+const LoginPage = lazy(() => import('@/pages/login'))
+const DashboardPage = lazy(() => import('@/pages/dashboard'))
+const HistoryPage = lazy(() => import('@/pages/history'))
+const ProfilePage = lazy(() => import('@/pages/profile'))
+const SettingsPage = lazy(() => import('@/pages/settings'))
+const RegisterPage = lazy(() => import('@/pages/register'))
 
 export const AppRouter = () => {
     return (
-        <Switch>
-            <Route path="/" component={HomePage} />
-            <Route path="/login/*?" component={LoginPage} />
+        <Suspense fallback={<FullPageLoader />}>
+            <Switch>
+                <Route path="/" component={HomePage} />
+                <Route path="/login/*?" component={LoginPage} />
 
-            <Route path="/app" nest>
-                <Route path="/" component={DashboardPage} />
-                <Route path="/history" component={HistoryPage} />
-                <Route path="/profile/*?" component={ProfilePage} />
-                <Route path="/settings" component={SettingsPage} />
-                <Route path="/register/*?" component={RegisterPage} />
-                <Navigation />
-            </Route>
-            {/* default 404 page */}
-            <Route>404, Not Found!</Route>
-        </Switch>
+                <Route path="/app" nest>
+                    <Route path="/" component={DashboardPage} />
+                    <Route path="/history" component={HistoryPage} />
+                    <Route path="/profile/*?" component={ProfilePage} />
+                    <Route path="/settings" component={SettingsPage} />
+                    <Route path="/register/*?" component={RegisterPage} />
+                    <Navigation />
+                </Route>
+                {/* default 404 page */}
+                <Route>404, Not Found!</Route>
+            </Switch>
+        </Suspense>
     )
 }
